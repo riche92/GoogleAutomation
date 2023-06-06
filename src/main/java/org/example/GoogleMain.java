6c695297d15abed106102897c416791f311b48fd
Rich Eng <richardeng92@gmail.com>
2023-06-05 21:10:34 -0400

    Google Automation 1st Update

---
+++ src/main/java/org/example/GoogleMain.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class GoogleMain {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/wild_/OneDrive/Documents/chromedriver.exe");
+        googleMethods Google = new googleMethods();
+        Google.login();
+
+
+    }
+}
