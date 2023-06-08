f1a4f27d1c1c8f8b9127c9edb1db626c0bf2f3fc
Rich Eng <richardeng92@gmail.com>
2023-06-08 15:18:36 -0400

    Commit via Intellij

--- src/main/java/org/example/googleMethods.java
+++ src/main/java/org/example/googleMethods.java
@@ -2,11 +2,16 @@ package org.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class googleMethods {
@@ -28,10 +33,28 @@ public class googleMethods {
         driver.navigate().to("https://google.com");
         }
         @Test
-        public void search(){
+        public void search() throws IOException {
+        SoftAssert soft = new SoftAssert();
         driver.findElement(By.xpath("//*[@type='search']")).sendKeys("Fried Chicken");
+       driver.findElement(By.xpath("//input[@value='Google Search']")).click();
+            List<WebElement> Links = driver.findElements(By.cssSelector("a"));
+            for (WebElement link : Links) {
+                //if()
+                String url = link.getAttribute("href");
+                HttpURLConnection connect = (HttpURLConnection) new URL(url).openConnection();
+                connect.setRequestMethod("HEAD");
+                connect.connect();
+                int respCode = connect.getResponseCode();
+                System.out.println(respCode);
+                soft.assertTrue(respCode<400, "The link with Text " + link.getText()+" is broken with code " + respCode);
+           /* if (respCode > 400) {
+                System.out.println("The link with Text " + link.getText()+" is broken with code " + respCode);
+                a.assertTrue(false);*/
+            }
+            soft.assertAll();
             System.out.println("Complete");
-
+            driver.quit();
 
         }
+
     }
