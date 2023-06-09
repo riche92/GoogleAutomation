00e962aab16922714cafe92ee47a14f0810f35ac
Rich Eng <richardeng92@gmail.com>
2023-06-08 23:08:08 -0400

    Link assertion fix

--- src/main/java/org/example/googleMethods.java
+++ src/main/java/org/example/googleMethods.java
@@ -18,43 +18,47 @@ public class googleMethods {
     WebDriver driver;
 
     @BeforeTest
-    public void setUp(){
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver", "/Users/wild_/OneDrive/Documents/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("https://google.com");
     }
+
     @Test(groups = {"Smoke"})
-        public void login(){
+    public void login() {
         driver.findElement(By.cssSelector("span[class = gb_Md]")).click();
         driver.findElement(By.xpath("//input[@type='email']")).sendKeys("richardeng92@gmail.com");
         driver.findElement(By.xpath("//*[@id='identifierNext']/div/button")).click();
         driver.navigate().to("https://google.com");
-        }
-        @Test
-        public void search() throws IOException {
+    }
+
+    @Test
+    public void search() throws IOException {
         SoftAssert soft = new SoftAssert();
         driver.findElement(By.xpath("//*[@type='search']")).sendKeys("Fried Chicken");
-       driver.findElement(By.xpath("//input[@value='Google Search']")).click();
-            List<WebElement> Links = driver.findElements(By.cssSelector("a"));
-            for (WebElement link : Links) {
-                //if()
-                String url = link.getAttribute("href");
+        driver.findElement(By.xpath("//input[@value='Google Search']")).click();
+        List<WebElement> Links = driver.findElements(By.cssSelector("a"));
+        for (WebElement link : Links) {
+            String url = link.getAttribute("href");
+            if (url != null) {
                 HttpURLConnection connect = (HttpURLConnection) new URL(url).openConnection();
                 connect.setRequestMethod("HEAD");
                 connect.connect();
                 int respCode = connect.getResponseCode();
                 System.out.println(respCode);
-                soft.assertTrue(respCode<400, "The link with Text " + link.getText()+" is broken with code " + respCode);
+                soft.assertTrue(respCode > 400, "The link with Text " + link.getText() + " is broken with code " + respCode);
+            }
+            else
+            {
+                System.out.println("No Link");
+            }
            /* if (respCode > 400) {
                 System.out.println("The link with Text " + link.getText()+" is broken with code " + respCode);
                 a.assertTrue(false);*/
-            }
-            soft.assertAll();
-            System.out.println("Complete");
-            driver.quit();
-
         }
-
+        driver.quit();
     }
+}
+
